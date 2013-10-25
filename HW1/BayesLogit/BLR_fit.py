7e5ed2039d03bcfeb40d16a55015eeba7768bb19
Chia Tung Kuo <chiatungkuo@gmail.com>
2013-10-24 19:27:31 -0700

    Add preliminary code to fit

--- HW1/BayesLogit/BLR_fit.py
+++ HW1/BayesLogit/BLR_fit.py
@@ -46,24 +46,58 @@ else:
 
 
 def bayes_logreg(m,y,X,beta_0,Sigma_0_inv,niter=10000,burnin=1000,print_every=1000,retune=100,verbose=False):
-	# Stuff...
-	return None
+        
+    def log_post_pdf(beta):
+        return (np.dot(np.dot(X, beta), y) - 0.5*np.dot(beta-beta_0, np.dot(Sigma_0_inv, beta-beta_0))) - \
+                np.dot(m, np.log(1+np.exp(np.dot(X, beta))))         
+
+    p = X.shape[1]  # dimension of x's
+    samples = np.zeros([niter+burnin, p])
+    samples[0, :] = np.zeros(p)
+                        
+    for t in xrange(1, niter+   burnin):
+            beta_new = np.random.multivariate_normal(samples[t-1, :], np.diag(np.ones(p)))
+            alpha = log_post_pdf(beta_new) - log_post_pdf(samples[t-1, :])
+            if np.log(np.random.rand()) < alpha:
+                    samples[t, :] = beta_new
+            else:
+                    samples[t, :] = samples[t-1, :]
+
+    return samples[burnin:, :]
 
 #################################################
+p = 2
 beta_0 = np.zeros(2)
 Sigma_0_inv = np.diag(np.ones(p))
 # More stuff...
 #################################################
 
 # Read data corresponding to appropriate sim_num:
+data_dir = 'data/'
+infile = open(data_dir + 'blr_data_' + str(sim_num) + '.csv', 'r')
 
-# Extract X and y:
+# Extract y, m and X:
+lines = infile.readlines()
+data = np.array(map(lambda x: x.rstrip('\n').split(','), lines[1:]), dtype='float')
+y = data[:, 0]
+m = data[:, 1]
+X = data[:, [2, 3]]
+infile.close()
 
 # Fit the Bayesian model:
+samples = bayes_logreg(m, y, X, beta_0, Sigma_0_inv, niter=100000, burnin=5000)
+# print samples.shape
+# exit()
 
 # Extract posterior quantiles...
+beta_percentiles = np.zeros([99, p])
+for i in range(99):
+    for r in range(p):
+        beta_percentiles[i, r] = np.percentile(samples[:, r], i+1)
 
 # Write results to a (99 x p) csv file...
+res_dir = 'results/'
+np.savetxt(res_dir + 'blr_res_' + str(sim_num) + '.csv', beta_percentiles, fmt='%.10f', delimiter=',')
 
 # Go celebrate.
  
