9fd3fc179b3ada3b661eb31f4559ee3cc5f70fdc
Chia Tung Kuo <chiatungkuo@gmail.com>
2013-11-14 18:43:05 -0800

    add floor to make sure negative bins are correct

--- HW2/Streaming/mapper.py
+++ HW2/Streaming/mapper.py
@@ -1,11 +1,12 @@
 #! /usr/bin/env python
 # Keys are x_bin,y_bin and value is 1 for each data point (x, y)
 import sys
+import numpy as np
 
 BIN_SIZE = 0.1
 
 for line in sys.stdin:
 	line = line.strip()
 	x, y = line.split()
-	x_bin, y_bin = int(float(x)/BIN_SIZE), int(float(y)/BIN_SIZE)
+	x_bin, y_bin = np.floor([float(x)/BIN_SIZE, float(y)/BIN_SIZE])
 	print '%s\t%s' % (str(x_bin) + ',' + str(y_bin), '1')
